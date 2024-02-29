6d2cb3928c60df5247091f454bc63cddab748f87
opilane <opilane@HP-CZC2349HTQ>
2024-02-29 14:56:38 +0200

    dadasdadada

--- mobile2/webbrowser.xaml.cs
+++ mobile2/webbrowser.xaml.cs
@@ -15,13 +15,25 @@ namespace mobile2
     {
         StackLayout st;
         Frame frame;
-        Picker picker;
+        Picker picker,picker2;
         WebView webView;
+        string sri = "";
+        Button bt,bt1,History_bt;
         string[] lehed = new string[3] { "https://www.tthk.ee/", "https://www.youtube.com/", "https://moodle.edu.ee/" };
         string[] nimetused = new string[3] { "tthk", "YOUTUBE", "Moodle" };
-        
+        string[] lehed1 = new string[4] { "https://www.tthk.ee/", "https://www.youtube.com/", "https://moodle.edu.ee/", "https://www.w3schools.com/" };
+        List<string> history = new List<string> {};
         public webbrowser()
         {
+            picker2 = new Picker
+            {
+                Title = "History",
+                 IsVisible = false
+            };
+            foreach (string skidish in history)
+            {
+                picker2.Items.Add(skidish);
+            }
             picker = new Picker
             {
                 Title = "Veebileht"
@@ -33,9 +45,11 @@ namespace mobile2
             webView = new WebView
             {
                 Source = new UrlWebViewSource { Url = "https://www.w3schools.com/" },
+                
                 HeightRequest = 500,
                 WidthRequest = 200,
             };
+            webView.Navigated += WebView_Navigated;
             frame = new Frame
             {
                 BorderColor =Color.Gray,
@@ -43,6 +57,28 @@ namespace mobile2
                 HeightRequest= 200,
                 WidthRequest= 200,
             };
+            bt = new Button
+            {
+                BorderColor = Color.Gray,
+                HeightRequest= 55,
+                WidthRequest= 30,
+                Text = "Koduleht"
+            };
+            bt1 = new Button
+            {
+                BorderColor = Color.Gray,
+                HeightRequest = 55,
+                WidthRequest = 30,
+                
+            };
+            History_bt = new Button
+            {
+                BorderColor = Color.Gray,
+                HeightRequest = 55,
+                WidthRequest = 30,
+            };
+
+            bt1.ImageSource = (@"pictures\back-modern-flat-icon-600nw-242284795.webp");
             SwipeGestureRecognizer swipe = new SwipeGestureRecognizer
             {
                 Direction = SwipeDirection.Right
@@ -59,17 +95,56 @@ namespace mobile2
             frame.GestureRecognizers.Add( swipe );
             frame.GestureRecognizers.Add( swipe1 );
             Content= st;
+            bt.Clicked += Home_page;
+            bt1.Clicked += Back_button;
+            History_bt.Clicked += historyView;
             swipe.Swiped += Swipe_Swiped;
             swipe1.Swiped+= Swipe_Swiped;
             //webView.GestureRecognizers.Add( swipe );
             picker.SelectedIndexChanged += Valime_leht_avamiseks;
+
             Content = new StackLayout
             {
-                Children = {picker, webView,frame}
+                Children = {picker,picker2,bt,bt1,History_bt,webView,frame,}
             };
         }
 
-        
+        private void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            if(e.Url == "https://www.w3schools.com/")
+            {
+                history.Add("https://www.w3schools.com/");
+            }
+            if (picker.SelectedIndex == 0)
+            {
+                history.Add("https://www.tthk.ee/");
+            }
+            if (picker.SelectedIndex == 1)
+            {
+                history.Add("https://www.youtube.com/");
+            }
+            if (picker.SelectedIndex == 2)
+            {
+                history.Add("https://moodle.edu.ee/");
+            }
+
+        }
+
+        private void historyView(object sender, EventArgs e)
+        {
+
+            webView.Source = new UrlWebViewSource { Url = history[picker2.SelectedIndex] };
+        }
+
+        private void Back_button(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Home_page(object sender, EventArgs e)
+        {
+            webView.Source = new UrlWebViewSource { Url = "https://www.w3schools.com/" };
+        }
 
         private void Swipe_Swiped(object sender, SwipedEventArgs e)
         {
@@ -101,6 +176,7 @@ namespace mobile2
         private void Valime_leht_avamiseks(object sender, EventArgs e)
         {
             webView.Source = new UrlWebViewSource { Url = lehed[picker.SelectedIndex] };
+                        
         }
     }
 }
